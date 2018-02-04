a7671909d336c6e5fb3945be2448e7d1f27526fe
techva <niaz.zaman@gmail.com>
2018-02-04 08:47:58 -0800

    update

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,7 +1,7 @@
 import random
 
 buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops','niaz','test')
+    'continuous deployment', 'continuous improvement', 'devops','auhona','test')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
